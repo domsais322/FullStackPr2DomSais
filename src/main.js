ccc05d00807ce5bd11c126ffdebd1ade0638a82e
domsais322 <128445878+domsais322@users.noreply.github.com>
2023-04-12 23:04:59 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
 import Vue from 'vue'
-import Vuetify from 'vuetify'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-Vue.use(Vuetify)
-createApp(App).mount('#app')
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faHatWizard)
+Vue.config.productionTip = false
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.use(vuetify);
+new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
